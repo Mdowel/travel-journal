1789aacc0e9a1eb8d119bd38e7693351fe6d4944
Mdowel <150044412+Mdowel@users.noreply.github.com>
2024-07-09 15:29:32 -0400

    test

---
+++ react-travel-journal/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>test</h1>
+      </header>
+    </div>
+  );
+}
+
+
